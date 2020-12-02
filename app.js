486597feeb0f72131f49e27abae388d27411619c
Jeppe Stenstrup Lauridsen <jepstrup@gmail.com>
2020-12-02 22:38:24 +0100

    Added res.end() to prevent program to stop sending requests after 6 numbers

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 'use strict';
-const { Client, Message } = require('node-osc');
+const { Client } = require('node-osc');
 var express = require("express"),
     app = express();
 
 app.use(express.static('public'))
 
-const OSC_ADDRESS = "/oscAddress"
+const OSC_PORT     = "127.0.0.1"
+
+const OSC_ADDRESS1 = "/d3/oscAddress1"
+const OSC_ADDRESS2 = "/d3/oscAddress2"
+const OSC_ADDRESS3 = "/d3/oscAddress3"
+const OSC_ADDRESS4 = "/d3/oscAddress4"
+const OSC_ADDRESS5 = "/d3/oscAddress5"
+const OSC_ADDRESS6 = "/d3/oscAddress6"
 
 app.get('/', (req, res) => { res.sendFile("index.html") })
 
 app.get('/:tal1/:tal2/:tal3/:tal4/:tal5/:tal6', (req, res) => {
-    const client = new Client('127.0.0.1', 3333);
-    client.send(OSC_ADDRESS, req.params.tal1, () => {
-        client.send(OSC_ADDRESS, req.params.tal2, () => {
-            client.send(OSC_ADDRESS, req.params.tal3, () => {
-                client.send(OSC_ADDRESS, req.params.tal4, () => {
-                    client.send(OSC_ADDRESS, req.params.tal5, () => {
-                        client.send(OSC_ADDRESS, req.params.tal6, () => {
+    const client = new Client(OSC_PORT, 3333);
+    client.send(OSC_ADDRESS1, parseFloat(req.params.tal1), () => {
+        client.send(OSC_ADDRESS2, parseFloat(req.params.tal2), () => {
+            client.send(OSC_ADDRESS3, parseFloat(req.params.tal3), () => {
+                client.send(OSC_ADDRESS4, parseFloat(req.params.tal4), () => {
+                    client.send(OSC_ADDRESS5, parseFloat(req.params.tal5), () => {
+                        client.send(OSC_ADDRESS6, parseFloat(req.params.tal6), () => {
                             client.close();
                         })
                     })
@@ -24,6 +31,7 @@ app.get('/:tal1/:tal2/:tal3/:tal4/:tal5/:tal6', (req, res) => {
             })
         })
     })
+    res.end()
 })
 
 app.listen(3000)
